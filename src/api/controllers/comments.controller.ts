78fc13c10ef17aa34ccc53d5a3fdc9bdda648f6c
Jose Latines <joselatines33@gmail.com>
2023-12-11 17:48:54 -0400

    fix(resource): comments

--- src/api/controllers/comments.controller.ts
+++ src/api/controllers/comments.controller.ts
@@ -3,29 +3,58 @@ import APIResponse from "../../interfaces/responses/APIResponse";
 import { Ratings } from "../../database/models/rating.model";
 import { handleServerError } from "../../errors/server.error";
 import { User } from "../../database/models/user.model";
+import { getUserFromToken } from "../../utils/jwt";
 export async function getAllComments(req: Request, res: Response<APIResponse>) {
-	try {
-		let comments: any = await Ratings.findAll({
-			order: [
-				["updatedAt", "ASC"],
-				["createdAt", "ASC"],
-			],
-			attributes: { exclude: ["rated_by"] },
-			include: [
-				{
-					model: User,
-					attributes: {
-						exclude: ["password", "role", "createdAt", "updatedAt", "provider"],
-					},
-				},
-			],
-		});
-		res
-			.status(200)
-			.json({ message: "Get all comments", success: true, data: comments });
-	} catch (error) {
-		handleServerError(res, error);
-	}
+  try {
+    const comments: any = await Ratings.findAll({
+      order: [
+        ["updatedAt", "ASC"],
+        ["createdAt", "ASC"],
+      ],
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ["password", "role", "createdAt", "updatedAt", "provider"],
+          },
+        },
+      ],
+    });
+
+    const authHeader: any = req.headers.authorization;
+    const user: any = getUserFromToken(authHeader);
+
+    if (!user) {
+      return res
+        .status(200)
+        .json({ message: "Get all comments", success: true, data: comments });
+    }
+
+    // Separate comments by whether they are rated by the user
+    const orderedComments = comments.reduce(
+      (acc: any, comment: any) => {
+        if (comment.rated_by === user.id) {
+          acc.unratedComments.unshift(comment);
+        } else {
+          acc.ratedComments.push(comment);
+        }
+        return acc;
+      },
+      { unratedComments: [], ratedComments: [] }
+    );
+
+    // Combine the unrated and rated comments
+    const finalOrderedComments = [
+      ...orderedComments.unratedComments,
+      ...orderedComments.ratedComments,
+    ];
+
+    return res
+      .status(200)
+      .json({ message: "Get all comments", success: true, data: finalOrderedComments });
+  } catch (error) {
+    handleServerError(res, error);
+  }
 }
 
 export async function deleteRating(req: Request, res: Response<APIResponse>) {
